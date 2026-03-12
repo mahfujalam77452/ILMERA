2fc10c238a3686fa29a4bb2e22b8dbd65b26b4cf
Md. Mahfuj Alam <mahfujalam77452@gmail.com>
2026-03-12 19:03:25 +0600

    Updated project files

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    console.log(process.env.MONGODB_URI);
     const conn = await mongoose.connect(
       process.env.MONGODB_URI || "mongodb://localhost:27017/ilmera",
       {
